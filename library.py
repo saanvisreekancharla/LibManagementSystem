d446cf745076a862b60a0905978830ce45715fb4
Saanvisreekancharla <kancharla.saanvi2023@vitstudent.ac.in>
2026-01-06 23:25:15 +0530

    modified login message in conflict branch

--- library.py
+++ library.py
@@ -1,7 +1,2 @@
-print("lib management system")
-
-def add_book():
-    print("Book added successfully")
-
 def login_user():
-    print("User logged in successfully")
+    print("User logged in successfully from conflict branch")
